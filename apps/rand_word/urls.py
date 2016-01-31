aa76df0a54c7bcad638a1ad48136a786b1828396
austinbrovick <austinbrovick@gmail.com>
2016-01-31 14:59:11 -0800

    first commit

---
+++ apps/rand_word/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url, include     # import functions to match patterns
+from . import views                    # import views.py file from our quiz app
+urlpatterns = patterns('',
+  # this url pattern matches empty string!!!
+  url(r'^$', views.index, name='index'),
+
+)
+
+
